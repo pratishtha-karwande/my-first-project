118f244719b9cf0160ba85f9d55c469543067307
Pratishtha <2021mcw002@sggs.ac.in>
2022-12-18 11:17:46 +0530

    git command added

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ alert('hello dear')
 // git commands
 
 // git init --> Declare current directory as a git dire
+// git remote add origin https://github.com/pratishtha-karwande/my-first-project.git
 
-// git add . --> 
+
+// git add . --> adding current changes to stage
+// git commit -m "my changes of html" --> commit your local changes
+// git pull origin "branch name" --> pull remote changes
+// git push origin "branch name" --> push local changes to remote
